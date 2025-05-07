97972e78c29fd46fd2650beb2918f06150dfe377
alanza <lanza.andrea.19@gmail.com>
2025-05-07 19:16:14 +0200

    aggiunta maschera e shift

--- main.py
+++ main.py
@@ -13,20 +13,49 @@ def main():
     D = dct_base()
     
     r, l = ex2.shape
+    ex2=ex2-128
 
+    print("passa sta q")
+    q = int(input("Inserisci un numero intero compreso tra 1 e 100 "))
+    if(q<1 or q>100):
+        q = int(input("Inserisci un numero intero compreso tra 1 e 100 "))
+    if(q<=50):
+        q=(100-q)/50
+    else:
+        q= 50/q
+        
+         # puoi cambiarlo per regolare la compressione
+    Q = np.array([  # Matrice di quantizzazione JPEG standard 8x8
+        [16,11,10,16,24,40,51,61],
+        [12,12,14,19,26,58,60,55],
+        [14,13,16,24,40,57,69,56],
+        [14,17,22,29,51,87,80,62],
+        [18,22,37,56,68,109,103,77],
+        [24,35,55,64,81,104,113,92],
+        [49,64,78,87,103,121,120,101],
+        [72,92,95,98,112,100,103,99]
+    ])
+    Qq = Q * q
+    div=np.zeros((8,8))
+    for i in range(0,8):
+        for j in range(0,8):
+            div= 1/(max(1,Qq[i,j]))
 
     f = np.zeros((r, l))
     for i in range(0, r, 8):
         for j in range(0, l, 8):
             submatrix = ex2[i:i+8, j:j+8]
             f[i:i+8, j:j+8]= dct2(submatrix, D)
+            np.multiply(f[i:i+8, j:j+8], div)
+    
 
-   
+    
     for i in range(0, r, 8):
         for j in range(0, l, 8):
             submatrixC = f[i:i+8, j:j+8]
             f[i:i+8, j:j+8]= idct2(submatrixC, D)
-        
+    
+    f=f+128
     print("f")
     print(f.shape)
     print(np.round(f, 2))
