c50f3637a8adf65fb0449bc87dee59191d4992d7
FerrarioChristian <chri.fer.emi@gmail.com>
2025-05-22 21:32:45 +0200

    Restored main.py, added gui.py
    * fixed file extension format

--- main.py
+++ main.py
@@ -1,131 +1,147 @@
-from tkinter import *
-from tkinter import filedialog
-from tkinter import messagebox
-from PIL import Image, ImageTk
-from backend import load_image, backend_func
+# from sys import maxsize as sys_maxsize
+
+import cv2
 import numpy as np
 
-
-def loadImage(root, img_label):
-    file_path = filedialog.askopenfilename(
-        title="Seleziona immagine", 
-        filetypes=[("Immagini", "*.png;*.jpg;*.jpeg;*.gif;*.bmp")], 
-        initialdir="./images"
-    )
-    
-    if file_path:
-        root.selected_file = file_path
-        img = Image.open(file_path)
-        
-        img = img.resize((600, 600))
-        img_tk = ImageTk.PhotoImage(img)
-        
-        img_label.config(image=img_tk)
-        img_label.image = img_tk  
-
-
-def loadImage_result(img_label_result):
-    file_path = "./images/result.bmp"
-    
-    if file_path:
-        img = Image.open(file_path)
-        
-        img = img.resize((600, 600))  
-        img_tk = ImageTk.PhotoImage(img)
-        
-        img_label_result.config(image=img_tk)
-        img_label_result.image = img_tk  
-
-
-def startConversion(fBox, dBox, root, img_label_result):
-    # Prendi i valori dai campi di testo
-
-    if not fBox.get().strip() or not dBox.get().strip():
-        messagebox.showerror("Errore", "Inserisci valori validi per f e d.")
-        return
-    f_value = int(fBox.get())
-    d_value = int(dBox.get())
-
-    print(f"Valori inseriti: {f_value}, {d_value}")
-    if not hasattr(root, 'selected_file') or root.selected_file is None:
-        messagebox.showerror("Errore", "Seleziona un'immagine prima di procedere.")
-        return
-    
-    # Verifica che i valori siano nel range corretto
-    if f_value <= 0 or d_value <= 0 or d_value > 2 * f_value - 2:
-        messagebox.showerror(
-            "Valori non validi",
-            f"Errore: f e d devono essere positivi, d deve essere minore di {2 * f_value - 2}"
-        )
-        return
-
-    try:
-        backend_func(root.selected_file, f_value, d_value)  # Chiama la funzione di conversione
-        loadImage_result(img_label_result)  # <-- carica l'immagine generata dopo la conversione
-    except Exception as e:
-        print(f"Errore durante la conversione: {e}")
+from dct import dct2, dct_base, idct2
 
 
 def main():
-    root = Tk()
+    image = load_image("./images/test.bmp").astype(np.float32)
 
-    root.title("Caravaggio-converter")
-    root.state('zoomed')
-    
-    # Imposta il background nero
-    #root.config(bg="grey")
-    
-    # Permetti il ridimensionamento della finestra
-    root.resizable(True, True)
+    f = input_value(f"Numero tra 1 e {min(image.shape)}: ", 1, min(image.shape))
+    d = input_value(f"Numero tra 0 e {2*f - 2}: ", 0, 2 * f - 2)
 
-    bg_img = Image.open("Caravaggio.jpg")
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    bg_img = bg_img.resize((screen_width, screen_height))
-    bg_img_tk = ImageTk.PhotoImage(bg_img)
+    # np.set_printoptions(threshold=sys_maxsize)
 
-    # Label per lo sfondo
-    bg_label = Label(root, image=bg_img_tk)
-    bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+    print_state("Immagine originale: ", image)
 
-    for i in range(3):
-        root.grid_columnconfigure(i, weight=1)
-    for i in range(2):
-        root.grid_rowconfigure(i, weight=10)
-    root.grid_rowconfigure(2, weight=1)
+    D = dct_base(f)
 
-    control_frame = Frame(root)
-    control_frame.grid(row=2, column=0, columnspan=3, padx=20, pady=10)
+    image = cut_image(image, f)
+    image = apply_dct(image, f, D)
+    image = cut_frequencies(image, f, d)
 
-    for i in range(6):
-        control_frame.grid_columnconfigure(i, weight=1)
+    print_state("Immagine dopo DCT: ", image)
 
-    img_label = Label(root)
-    img_label.grid(row=1, column=0)
+    image = apply_idct(image, f, D)
 
-    img_label_result = Label(root)
-    img_label_result.grid(row=1, column=2)
+    print_state("Immagine dopo IDCT: ", image)
 
-    f_label = Label(control_frame, text="Valore di f:")
-    f_label.grid(row=2, column=2,sticky="e")
+    image = np.clip(image, 0, 255).astype(np.uint8)
+    cv2.imwrite("result.bmp", image)
 
-    fBox = Entry(control_frame, width=20)
-    fBox.grid(row=2, column=3)
 
-    d_label = Label(control_frame,  text="Valore di d:")
-    d_label.grid(row=2, column=4,sticky="e")
+def apply_dct(image, f, D):
+    """
+    Applica la DCT a blocchi di dimensione f x f dell'immagine.
+    """
+    r, l = image.shape
+    image = image - 128
 
-    dBox = Entry(control_frame,  width=20)
-    dBox.grid(row=2, column=5)
+    for i in range(0, r, f):
+        for j in range(0, l, f):
+            if i + f <= r and j + f <= l:
+                image[i : i + f, j : j + f] = dct2(image[i : i + f, j : j + f], D)
 
-    caricaImmagine = Button(control_frame, text="Seleziona immagine", padx=25, command=lambda: loadImage(root, img_label))
-    caricaImmagine.grid(row=2, column=0, padx=15)
+    return image
 
-    conversionButton = Button(control_frame, text="Converti",   command=lambda: startConversion(fBox, dBox, root, img_label_result))
-    conversionButton.grid(row=2, column=1, padx=15)
 
-    root.mainloop()
+def apply_idct(image, f, D):
+    """
+    Applica la IDCT a blocchi di dimensione f x f dell'immagine.
+    """
+    r, l = image.shape
+
+    for i in range(0, r, f):
+        for j in range(0, l, f):
+            if i + f <= r and j + f <= l:
+                image[i : i + f, j : j + f] = idct2(image[i : i + f, j : j + f], D)
+
+    image = image + 128
+
+    return image
+
+
+def cut_frequencies(image, f, d):
+    div = np.zeros((f, f))
+    for i in range(f):
+        for j in range(f):
+            if (i + j) < d:
+                div[i, j] = 1
+
+    for i in range(0, image.shape[0], f):
+        for j in range(0, image.shape[1], f):
+            if i + f <= image.shape[0] and j + f <= image.shape[1]:
+                image[i : i + f, j : j + f] = np.multiply(
+                    image[i : i + f, j : j + f], div
+                )
+    return image
+
+
+def cut_image(image, f):
+    """
+    Taglia l'immagine in modo che le dimensioni siano multipli di f.
+    """
+    r, l = image.shape
+    r = r - (r % f)
+    l = l - (l % f)
+    image = image[0:r, 0:l]
+
+    return image
+
+
+def load_image(image_path):
+    """
+    Carica un'immagine in scala di grigi.
+    """
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    if image is None:
+        raise ValueError(f"Impossibile caricare l'immagine da {image_path}")
+    return image
+
+
+def input_value(msg, min, max):
+    while True:
+        try:
+            x = int(input(msg))
+            if x >= min and x <= max:
+                return x
+        except:
+            pass
+        print(f"Inserisci un numero intero tra {min} e {max}.")
+
+
+def print_state(msg, matrix):
+    """
+    Stampa lo stato della matrice.
+    """
+    print(msg)
+    print(f"{matrix.shape}")
+    print(matrix)
 
 
 if __name__ == "__main__":
     main()
+
+# Codice per la matrice di quantizzazione
+
+# if(q<=50):
+#     q=(100-q)/50
+# else:
+#     q= 50/q
+# Q = np.array([
+#     [16,11,10,16,24,40,51,61],
+#     [12,12,14,19,26,58,60,55],
+#     [14,13,16,24,40,57,69,56],
+#     [14,17,22,29,51,87,80,62],
+#     [18,22,37,56,68,109,103,77],
+#     [24,35,55,64,81,104,113,92],
+#     [49,64,78,87,103,121,120,101],
+#     [72,92,95,98,112,100,103,99]
+# ])
+# Qq = Q * q
+# div=np.zeros((8,8))
+# for i in range(0,8):
+#     for j in range(0,8):
+#         div= 1/(max(1,Qq[i,j]))
