b7b61173a89fcd74dc52665c6a5dd9627ce31ba9
FerrarioChristian <chri.fer.emi@gmail.com>
2025-06-05 17:13:57 +0200

    fixed benchmark

--- tests/parameters.py
+++ tests/parameters.py
@@ -1,22 +1,23 @@
 from converter import compress
 
-IMAGE = "./images/640x640.bmp"
-F = [4, 8, 16, 32, 64, 128]
+IMAGED = "./images/640x640.bmp"
+IMAGEF = "./images/deer.bmp"
+F = [8, 16, 32, 64, 128]
 
 D = [1, 4, 8, 12, 16, 18, 24, 60]
 
 
 def main():
     for f in F:
-        d = int(f / 3)
+        d = int(f / 5)
         print(f"Processing with f={f}, d={d}")
-        compress(IMAGE, f, d, "./results/parameters/f/f{}_d{}.bmp".format(f, d))
+        compress(IMAGEF, f, d, "./results/parameters/f/f{}_d{}.jpg".format(f, d))
         print(f"Completed processing with f={f}, d={d}\n")
 
     f = 64
     for d in D:
         print(f"Processing with f={f}, d={d}")
-        compress(IMAGE, f, d, "./results/parameters/d/d{}_f{}.bmp".format(d, f))
+        compress(IMAGED, f, d, "./results/parameters/d/d{}_f{}.jpg".format(d, f))
         print(f"Completed processing with f={f}, d={d}\n")
 
 
